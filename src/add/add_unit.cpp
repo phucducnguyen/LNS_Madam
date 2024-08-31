1eb0ee19b4316e889fd297a24cef3a8b4fd12cfb
PhucDucNguyen <pnguyen2101@gmail.com>
2024-08-30 22:31:16 -0700

    update add

--- src/add/add_unit.cpp
+++ src/add/add_unit.cpp
@@ -4,8 +4,10 @@
 void sorting_unit(quotient_t quotient, remainder_t remainder, hls::stream<sum_t> partial_sums[N]) {
 #pragma HLS INLINE
 #pragma HLS ARRAY_PARTITION variable=partial_sums complete dim=1
-    sum_t quotient_contribution = (1 << quotient); // Generate contribution by shifting 1 by quotient value
-    partial_sums[remainder].write(quotient_contribution); // Route contribution to the correct partial sum accumulator
+    // Generate contribution by shifting 1 by quotient value
+    sum_t quotient_contribution = (1 << quotient);
+    // Route contribution to the correct partial sum accumulator
+    partial_sums[remainder].write(quotient_contribution);
 }
 
 // Partial Sums Accumulator
@@ -20,7 +22,35 @@ void partial_sum_accumulator(hls::stream<sum_t> partial_sums[N], sum_t &result_s
 }
 
 // Partial Sums Generation Unit
-void add(quotient_t quotients[N], remainder_t remainders[N], sum_t &final_sum) {
+// void add(quotient_t quotients[N], remainder_t remainders[N], sum_t &final_sum) {
+// #pragma HLS DATAFLOW
+
+//     hls::stream<sum_t> partial_sums[N];
+// #pragma HLS STREAM variable=partial_sums depth=2
+
+//     // Sorting and accumulation of partial sums
+//     for (int i = 0; i < N; i++) {
+// #pragma HLS UNROLL
+//         sorting_unit(quotients[i], remainders[i], partial_sums); // Sort quotient contributions
+//     }
+
+//     partial_sum_accumulator(partial_sums, final_sum); // Accumulate the sorted partial sums
+// }
+
+void addition_unit(
+    sum_t partial_sum_accumulator_out[N], 
+    sum_t &final_sum
+) {
+#pragma HLS INLINE
+    sum_t sum = 0;
+    for (int i = 0; i < N; i++) {
+#pragma HLS UNROLL
+        sum += partial_sum_accumulator_out[i];
+    }
+    final_sum = sum;
+}
+
+void partial_sums_generation_unit(quotient_t quotients[N], remainder_t remainders[N], sum_t &final_sum) {
 #pragma HLS DATAFLOW
 
     hls::stream<sum_t> partial_sums[N];
@@ -32,7 +62,14 @@ void add(quotient_t quotients[N], remainder_t remainders[N], sum_t &final_sum) {
         sorting_unit(quotients[i], remainders[i], partial_sums); // Sort quotient contributions
     }
 
-    partial_sum_accumulator(partial_sums, final_sum); // Accumulate the sorted partial sums
+    sum_t partial_sum_accumulator_out[N];
+
+    for (int i = 0; i < N; i++) {
+#pragma HLS UNROLL
+        partial_sum_accumulator(partial_sums[i], partial_sum_accumulator_out[i]);
+    }
+
+    addition_unit(partial_sum_accumulator_out, final_sum); // Add up the partial sums
 }
 
 
